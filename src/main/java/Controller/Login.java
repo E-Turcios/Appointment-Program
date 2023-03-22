86d0bcb83f4746bfd5566e63dea7afc1c836ef9e
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-03-21 21:55:35 -0400

    second commit

---
+++ src/main/java/Controller/Login.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginLabel;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private Label timeZoneDisplay;
+
+    @FXML
+    private Label timeZoneLabel;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TextField usernameText;
+
+    @FXML
+    void exitOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginOnClick(ActionEvent event) {
+
+    }
+
+}
